fd0e8256f7db720d6173e230af953208e45da705
Wellington Caetano <wellington.caetano@portalsolar.com.br>
2021-06-23 01:59:47 -0300

    New room improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 
 function App() {
@@ -12,8 +13,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact={true} component={Home}></Route>
-        <Route path="/rooms/new" component={NewRoom}></Route>
+        <Switch>
+          <Route path="/" exact={true} component={Home}></Route>
+          <Route path="/rooms/new" exact component={NewRoom}></Route>
+          <Route path="/rooms/:id"component={Room}></Route>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
